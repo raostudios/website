c8a04d282d7338dcdd032bb1da001b2a18985ce9
Venkat S. Rao <venkat@raostudios.com>
2019-04-09 09:33:59 -0400

    layout updates

--- src/pages/preset.js
+++ src/pages/preset.js
@@ -1,10 +1,12 @@
 import React from "react"
 import AppPageLayout from "../layouts/app-page-layout"
 
+const ImageShowcase = props => (
+  <div></div>
+)
+
 export default () => (
   <AppPageLayout name = 'preset' tintColor='#545345'>
-    <p>
-      Content
-    </p>
+    <ImageShowcase />
   </AppPageLayout>
 )
